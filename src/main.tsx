736bc7681b099db316bc0365869d824f92145e04
BarWanunu <barwanunu6@gmail.com>
2025-02-08 16:59:40 +0200

    Implement seaching

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ColorModeScript } from "@chakra-ui/system";
 import App from "./App";
+import "./index.css";
 import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
