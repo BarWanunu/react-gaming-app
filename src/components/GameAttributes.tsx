2d0a8bc30cf226e7cde2f6554438b09f609a150a
BarWanunu <barwanunu6@gmail.com>
2025-02-10 19:53:01 +0200

    Build game attributes

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,38 @@
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import DefenitionItem from "./DefenitionItem";
+import CriticScore from "./CriticScore";
+import { Game } from "../entities/Game";
+
+interface Props {
+  game: Game;
+}
+
+const GameAttributes = ({ game }: Props) => {
+  return (
+    <>
+      <SimpleGrid columns={2} as={"dl"}>
+        <DefenitionItem term="Platforms">
+          {game.parent_platforms?.map(({ platform }) => (
+            <Text key={platform.id}>{platform.name}</Text>
+          ))}
+        </DefenitionItem>
+        <DefenitionItem term="Metascore">
+          <CriticScore score={game.metacritic} />
+        </DefenitionItem>
+
+        <DefenitionItem term="genres">
+          {game.genres.map((genre) => (
+            <Text key={genre.id}>{genre.name}</Text>
+          ))}
+        </DefenitionItem>
+        <DefenitionItem term="Publishers">
+          {game.publishers?.map((p) => (
+            <Text key={p.id}>{p.name}</Text>
+          ))}
+        </DefenitionItem>
+      </SimpleGrid>
+    </>
+  );
+};
+
+export default GameAttributes;
